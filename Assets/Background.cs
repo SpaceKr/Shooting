04e9c3986870cbe75b926ab0630950b3512969f5
SpaceKr <147888789+SpaceKr@users.noreply.github.com>
2023-10-14 13:51:18 +0900

    10/14 작업
    배경 스크롤,플레이어 이동 구현

---
+++ Assets/Background.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform[] bottom;
+    [SerializeField] private Transform[] middle;
+    [SerializeField] private Transform[] top;
+    [SerializeField] private float bottomSpeed;
+    [SerializeField] private float middleSpeed;
+    [SerializeField] private float topSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var item in bottom)
+        {
+            item.transform.Translate(Vector2.down * Time.deltaTime * bottomSpeed);
+            if (item.transform.position.y < -12)
+            {
+                item.transform.position = new Vector2(0f, 12f);
+            }
+        }
+        foreach (var item in middle)
+        {
+            item.transform.Translate(Vector2.down * Time.deltaTime * middleSpeed);
+            if (item.transform.position.y < -12)
+            {
+                item.transform.position = new Vector2(0f, 12f);
+            }
+        }
+        foreach (var item in top)
+        {
+            item.transform.Translate(Vector2.down * Time.deltaTime * topSpeed);
+            if (item.transform.position.y < -12)
+            {
+                item.transform.position = new Vector2(0f, 12f);
+            }
+        }
+    }
+}
---
+++ Assets/SpriteAnimation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class SpriteAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer sr;
+    private List<Sprite> sprite;
+    private float delay;
+    int animindex = 0;
+    float timer;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sprite.Count == 0)
+        {
+            return;
+        }timer += Time.deltaTime;
+        if (timer >= delay)
+        {
+            timer = 0;
+            sr.sprite = sprite[animindex];
+            animindex++;
+            if (sprite.Count <= animindex)
+            {
+                animindex = 0;
+            }
+        }
+        
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SetSprite(List<Sprite> sprite,float delay)
+    {
+        this.sprite = sprite;
+        this.delay = delay;
+        sr.sprite = this.sprite[0];
+        animindex = 1;
+    }
+}
